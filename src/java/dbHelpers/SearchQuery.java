123586401855c47b667d8b10b4cf57ebf3d23610
Amber Gerard <amber-j-gerard@uiowa.edu>
2016-10-28 10:07:55 -0500

    created searchservlet

--- src/java/dbHelpers/SearchQuery.java
+++ src/java/dbHelpers/SearchQuery.java
@@ -11,6 +11,7 @@ import java.sql.SQLException;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import model.Pokemon;
 
 
 public class SearchQuery {
@@ -60,6 +61,88 @@ public class SearchQuery {
         } catch (SQLException ex) {
             Logger.getLogger(SearchQuery.class.getName()).log(Level.SEVERE, null, ex);
         }
+    
+    }
+    
+    public String getHTMLtable(){
+     
+        String table = "";
+        
+        table += "<table>";
+        
+        table += "<tr>";
+        table += "<th>";
+        table += "PokemonID";
+        table += "</th>";
+        
+        table += "<th>";
+        table += "Pokemon Name";
+        table += "</th>";
+        
+        table += "<th>";
+        table += "Type";
+        table += "</th>";
+        
+        table += "<th>";
+        table += "Location";
+        table += "</th>";
+        
+        table += "<th>";
+        table += "Strength Rating";
+        table += "</th>";
+        
+        table += "<th>";
+        table += "";
+        table += "</th>";
+        table += "</tr>";
+              
+        
+        try {
+            while(this.results.next()){
+                
+                Pokemon pokemon = new Pokemon();
+                pokemon.setPokemonID(this.results.getInt("PokemonID"));
+                pokemon.setPokemonName(this.results.getString("PokemonName"));
+                pokemon.setType(this.results.getString("Type"));
+                pokemon.setLocation(this.results.getString("Location"));
+                pokemon.setStrengthRating(this.results.getInt("StrengthRating"));
+                
+                table += "<tr>";
+                table += "<td>";
+                table += pokemon.getPokemonID();
+                table += "</td>" ; 
+                
+                table += "<td>";
+                table += pokemon.getPokemonName();
+                table += "</td>" ;    
+                
+                table += "<td>";
+                table += pokemon.getType();
+                table += "</td>" ;    
+                
+                table += "<td>";
+                table += pokemon.getLocation();
+                table += "</td>" ;    
+                
+                table += "<td>";
+                table += pokemon.getStrengthRating();
+                table += "</td>" ;   
+                
+                table += "<td>";
+                table += "<a href=update?PokemonID=" + pokemon.getPokemonID() + "> Update </a>" + "<a href=delete?PokemonID=" + pokemon.getPokemonID() +"> Delete </a>";
+                table += "</td>";
+                
+                table += "</tr>";
+                
+               
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(SearchQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        table += "</table>";
+        
+        return table;
+    
     
     }
     
